966ac10fc80c4bc129c95e789e4d1f834c5601d5
Varun Shringarpure <varun.vns@gmail.com>
2016-11-04 14:33:07 +0530

    Intial Files

---
+++ Sitecore.ResetItemFields.WithTDS/Sitecore.ResetItemFields/Sitecore.ResetItemFields/sitecore/admin/ResetFields/Assets/ResetItemFields.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ResetItemFields.aspx.cs" Inherits="Sitecore.ResetItemFields.sitecore.admin.ResetFields.Assets.ResetItemFields" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Sitecore.ResetItemFields.WithTDS/Sitecore.ResetItemFields/Sitecore.ResetItemFields/sitecore/admin/ResetFields/Assets/ResetItemFields.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sitecore.ResetItemFields.sitecore.admin.ResetFields.Assets
+{
+    public partial class ResetItemFields : Sitecore.sitecore.admin.AdminPage
+    {
+        protected override void OnInit(EventArgs e)
+        {
+            base.CheckSecurity(true); 
+            base.OnInit(e);
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
